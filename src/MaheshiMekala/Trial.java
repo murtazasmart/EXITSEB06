004c67ba923070d5c356767f94e52d4a135271b7
murtazasmart <murtazasmart@hotmail.com>
2017-05-28 13:31:39 +0530

    g

---
+++ src/MaheshiMekala/Trial.java
@@ -0,0 +1,11 @@
+package MaheshiMekala;
+
+/**
+ * Created by MA_Laptop on 5/28/2017.
+ */
+public class Trial {
+
+    public static void main(String[] args) {
+        System.out.println("Im Maheshi");
+    }
+}
