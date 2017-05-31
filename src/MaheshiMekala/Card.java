dcabc09c2b6b67e6fed3da683a2bcf29673d083e
ASUS\asus i3 <mekala.rashmika@gmail.com>
2017-05-31 06:30:18 +0530

    s

--- src/MaheshiMekala/Card.java
+++ src/MaheshiMekala/Card.java
@@ -12,99 +12,99 @@ public class Card {
 
 
 
-    public void xx(){
-        Queue cardq= new Queue() {
-            @Override
-            public boolean add(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean offer(Object o) {
-                return false;
-            }
-
-            @Override
-            public Object remove() {
-                return null;
-            }
-
-            @Override
-            public Object poll() {
-                return null;
-            }
-
-            @Override
-            public Object element() {
-                return null;
-            }
-
-            @Override
-            public Object peek() {
-                return null;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public Object[] toArray(Object[] objects) {
-                return new Object[0];
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection collection) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection collection) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection collection) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection collection) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-        }
-    }
+//    public void xx(){
+//        Queue cardq= new Queue() {
+//            @Override
+//            public boolean add(Object o) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean offer(Object o) {
+//                return false;
+//            }
+//
+//            @Override
+//            public Object remove() {
+//                return null;
+//            }
+//
+//            @Override
+//            public Object poll() {
+//                return null;
+//            }
+//
+//            @Override
+//            public Object element() {
+//                return null;
+//            }
+//
+//            @Override
+//            public Object peek() {
+//                return null;
+//            }
+//
+//            @Override
+//            public int size() {
+//                return 0;
+//            }
+//
+//            @Override
+//            public boolean isEmpty() {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean contains(Object o) {
+//                return false;
+//            }
+//
+//            @Override
+//            public Iterator iterator() {
+//                return null;
+//            }
+//
+//            @Override
+//            public Object[] toArray() {
+//                return new Object[0];
+//            }
+//
+//            @Override
+//            public Object[] toArray(Object[] objects) {
+//                return new Object[0];
+//            }
+//
+//            @Override
+//            public boolean remove(Object o) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean containsAll(Collection collection) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean addAll(Collection collection) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean removeAll(Collection collection) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean retainAll(Collection collection) {
+//                return false;
+//            }
+//
+//            @Override
+//            public void clear() {
+//
+//            }
+//        };
+//    }
 
     public String[] shuffel()
     {
