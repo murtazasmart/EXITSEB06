de16e769646bb6f35934741ef3fe5024ccde7208
BhagyaR94 <rathnayake.bhagya94@gmail.com>
2017-06-01 13:12:43 +0530

    test commit

---
+++ src/MediaControllers/MediaPlayerController.java
@@ -0,0 +1,7 @@
+package MediaControllers;
+
+/**
+ * Created by Bhagya Rathnayake on 6/1/2017.
+ */
+public class MediaPlayerController {
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,18 +4,36 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.time.Duration;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../startupscreen/startup.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("EXIT-POKER");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.setMaximized(true);
         primaryStage.show();
+try{
 
+
+//    Media sound = new Media();
+//    MediaPlayer player = new MediaPlayer(sound);
+//    player.setCycleCount(MediaPlayer.INDEFINITE);
+//    player.play();
+}
+
+catch(Exception e){
+    System.out.println(e);
+}
     }
 
 
