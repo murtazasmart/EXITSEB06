8d7d0b1a67d928299060c3d077a64a5c8f7b74a8
ASUS\asus i3 <mekala.rashmika@gmail.com>
2017-06-21 01:07:06 +0530

    player and reffree obj 80% complet

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -6,103 +6,358 @@ import java.io.Serializable;
  * Created by MA_Laptop on 6/18/2017.
  */
 public class Player implements Serializable {
-    String username;
-    int score;
-    String[][] otherPlayerCards;
 
-    public Player() {
+
+    private String username;
+    boolean isKicked;
+    private int numberofplayers;
+    private boolean swapCards[];
+    int roundARisk;
+    int roundBRisk;
+    private int playerId;
+
+
+    /**
+     * @return the numberofplayers
+     */
+    public int getNumberofplayers() {
+        return numberofplayers;
     }
 
-    public Player(ClientThread clientThread){
-        username = clientThread.username;
-
+    /**
+     * @param numberofplayers the numberofplayers to set
+     */
+    public void setNumberofplayers(int numberofplayers) {
+        this.numberofplayers = numberofplayers;
     }
 
+    /**
+     * @return the swapCards
+     */
+    public boolean[] getSwapCards() {
+        return swapCards;
+    }
+
+    /**
+     * @param swapCards the swapCards to set
+     */
+    public void setSwapCards(boolean[] swapCards) {
+        this.swapCards = swapCards;
+    }
+
+//   Refree re=new Refree();
+//   int playersc=re.StartGame();
+//   String[][] playerHand=null;
+//
+//   public String[][] displayHand(){
+//        playerHand=re.DistributeCard(5);
+//        for(int j=0;j<5;j++){
+//            System.out.print("player "+j+" owned cards:");
+//            for (int i=0;i<5;i++) {
+//                System.out.print(""+playerHand[j][i]+",");
+//
+//            }
+//            System.out.println("");
+//        }
+//        //display user1 hand
+//        for (int j=0;j<5;j++){
+//            for (int i=0;i<5;i++){
+//              System.out.print("\n user" +(j+1)+"  card "+playerHand[j][i]+"\n");
+//        }
+//        }
+//
+//        //end of for loop
+//        return playerHand;
+//   }// end of display hand
+//
+//
+//   Card cr=new Card();
+//   int point=0;
+//   public int GetType(String[][] array){
+//
+//
+//        String type=null;
+////
+//            array=playerHand;
+////       // array=displayHand();
+//        int sum=0;
+//    for(int j=0;j<5;j++){
+//        for(int i=0;i<5;i++){
+////
+//            type=array[j][i].substring(0,playerHand[j][i].indexOf("-"));
+////
+//           System.out.println("user "+ j+" card type= "+type);
+//
+//       switch(type){
+//
+//           case "s":
+//               point=5;
+//               //sum+=point;
+//           break;
+//
+//           case "d":
+//               point=4;
+//               //sum+=point;
+//           break;
+//
+//           case "c":
+//               point=3;
+//               //sum+=point;
+//           break;
+//
+//           case "h":
+//               point=2;
+//               //sum+=point;
+//           break;
+//
+//       }
+//              sum+=point;
+//
+//       }
+//
+//           //sum+=point;
+//           System.out.println("point"+point);
+//           System.out.println("sum "+sum);
+//            }
+//
+//     return sum;
+//}
+//
+//
+//
+//
+//   public String GetValue(String[][] array){
+//
+//
+//        String value=null;
+//            array=playerHand;
+//
+//    for(int j=0;j<5;j++){
+//        for(int i=0;i<5;i++){
+//
+//            value=array[j][i].substring(playerHand[j][i].indexOf("-")+1);
+//
+//           System.out.println("user "+ j+ " card value= "+value);
+//
+//        }
+//}
+//
+//            return value;
+//
+//  }
+//
+//
+//   //get the username from userid
+//   public String[][] user_info(){
+//
+//        String[][] userDetail=new String[1][4];//for 5 users
+//
+//        // i dont how u guys get those user is and stuff fill this and return it
+//
+//
+//
+//    return null;
+//}
+//
+//
+//   //2d array to keep the cards to change
+//   public String[][] changeHand(){
+//
+//   String[][] change=new  String[4][3]; //maximum 3 cards to be change
+//
+//
+//
+//
+//       return null;
+//   }
+
+
+
+
+    //initial card hand
+
+
+    private String[][] CardHand=new String[numberofplayers][5];
+
+    public String[][] getCardHand(){
+        return CardHand;
+
+    }
+    public void setCardHand(String[][] CardHand){
+        this.CardHand=CardHand;
+    }
+
+    public void setIndividualCardHand(int user, int cardindx, String cardval){
+        this.CardHand[user][cardindx]=cardval;
+    }
+    public String getIndividualCardHand(int user, int cardindx){
+        return CardHand[user][cardindx];
+    }
+
+    //score
+    private int Score;
+    public int getScore(){
+        return Score;
+    }
+
+    public void setScore(int Score){
+        this.Score=Score;
+    }
+
+    //to keep the username for eac user id
+    private String[] user_info=new String[numberofplayers];
+
+    public String[] getUser_info(){
+        return user_info;
+    }
+    public void setUser_info(String[] user_info){
+        this.user_info=user_info;
+    }
+
+    //change card hand
+    private String[] ChangeHand=new String[5];//private String[][] ChangeHand=new String[numberofplayers][5];
+
+    public String[] getChangeHand(){//public String[][] getChangeHand(){
+        return ChangeHand;
+    }
+
+    public void setChangeHand(String[] ChangeHand){//public void setChangeHand(String[][] ChangeHand){
+        this.ChangeHand=ChangeHand;
+    }
+
+    /**
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * @param username the username to set
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public int getScore() {
-        return score;
+    void getObjectReady(int playerscount) {
+        swapCards = new boolean[playerscount];
     }
 
-    public void setScore(int score) {
-        this.score = score;
+    /**
+     * @return the playerId
+     */
+    public int getPlayerId() {
+        return playerId;
     }
 
-    Referee re=new Referee();
-    //int playersc=re.StartGame();
-    String[][] playerHand=null;
-
-    public String[][] displayHand(){
-        playerHand=re.DistributeCard(5);
-        for(int j=0;j<5;j++){
-            System.out.print("player "+j+" owned cards:");
-            for (int i=0;i<5;i++) {
-                System.out.print(""+playerHand[j][i]+",");
-            }
-            System.out.println("");
-        }
-        //display user1 hand
-        for (int i=0;i<5;i++){
-            System.out.print("user 1 card "+playerHand[0][i]);
-        }
-        for (int i=0;i<5;i++){
-            System.out.print(" user 2 card "+playerHand[1][i]);
-
-        }
-        for (int i=0;i<5;i++){
-            System.out.print("user 3 card "+playerHand[2][i]);
-        }
-        for (int i=0;i<5;i++){
-            System.out.print("user 4 card "+playerHand[3][i]);
-        }
-        for (int i=0;i<5;i++){
-            System.out.print("user 5 card "+playerHand[4][i]);
-        }
-
-        //end of for loop
-        return playerHand;
-    }// end of display hand
-
-    //declaring point array
-    public  int[][] marks = { {20,19,18,17},
-            {19,18,17,16},
-            {18,17,16,15},
-            {17,16,15,14},
-            {16,15,14,13},
-            {15,14,13,12},
-            {14,13,12,11},
-            {12,11,10,9},
-            {11,10,9,8},
-            {10,9,8,7},
-            {9,8,7,6},
-            {8,7,6,5} };
-    Card cr=new Card();
-
-    public String splitCard(String[][] array){
-
-        //array=new String[]{"s-2","d-10","h-6","s-4","s-9"};
-        String type=null;
-        String value=null;
-        array=displayHand();
-        for(int i=0;i<5;i++){
-            //String[] word=pack[i].split("-");
-            type=array[0][i].substring(0,cr.pack[i].indexOf("-"));
-            value=array[0][i].substring(cr.pack[i].indexOf("-")+1);
-            //String type=word[i];
-            System.out.println("type="+type);
-            System.out.println("value="+value);
-           /* for(String result:word){
-            System.out.println(result);
-            }*/
-        }
-
-        return type;
-
+    /**
+     * @param playerId the playerId to set
+     */
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
     }
+
+
+
+
+
+//    String username;
+//    int score;
+//    String[][] otherPlayerCards;
+//
+//    public Player() {
+//    }
+//
+//    public Player(ClientThread clientThread){
+//        username = clientThread.username;
+//
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public int getScore() {
+//        return score;
+//    }
+//
+//    public void setScore(int score) {
+//        this.score = score;
+//    }
+//
+//    Referee re=new Referee();
+//    //int playersc=re.StartGame();
+//    String[][] playerHand=null;
+//
+//    public String[][] displayHand(){
+//        playerHand=re.DistributeCard(5);
+//        for(int j=0;j<5;j++){
+//            System.out.print("player "+j+" owned cards:");
+//            for (int i=0;i<5;i++) {
+//                System.out.print(""+playerHand[j][i]+",");
+//            }
+//            System.out.println("");
+//        }
+//        //display user1 hand
+//        for (int i=0;i<5;i++){
+//            System.out.print("user 1 card "+playerHand[0][i]);
+//        }
+//        for (int i=0;i<5;i++){
+//            System.out.print(" user 2 card "+playerHand[1][i]);
+//
+//        }
+//        for (int i=0;i<5;i++){
+//            System.out.print("user 3 card "+playerHand[2][i]);
+//        }
+//        for (int i=0;i<5;i++){
+//            System.out.print("user 4 card "+playerHand[3][i]);
+//        }
+//        for (int i=0;i<5;i++){
+//            System.out.print("user 5 card "+playerHand[4][i]);
+//        }
+//
+//        //end of for loop
+//        return playerHand;
+//    }// end of display hand
+//
+//    //declaring point array
+//    public  int[][] marks = { {20,19,18,17},
+//            {19,18,17,16},
+//            {18,17,16,15},
+//            {17,16,15,14},
+//            {16,15,14,13},
+//            {15,14,13,12},
+//            {14,13,12,11},
+//            {12,11,10,9},
+//            {11,10,9,8},
+//            {10,9,8,7},
+//            {9,8,7,6},
+//            {8,7,6,5} };
+//    Card cr=new Card();
+//
+//    public String splitCard(String[][] array){
+//
+//        //array=new String[]{"s-2","d-10","h-6","s-4","s-9"};
+//        String type=null;
+//        String value=null;
+//        array=displayHand();
+//        for(int i=0;i<5;i++){
+//            //String[] word=pack[i].split("-");
+//            type=array[0][i].substring(0,cr.pack[i].indexOf("-"));
+//            value=array[0][i].substring(cr.pack[i].indexOf("-")+1);
+//            //String type=word[i];
+//            System.out.println("type="+type);
+//            System.out.println("value="+value);
+//           /* for(String result:word){
+//            System.out.println(result);
+//            }*/
+//        }
+//
+//        return type;
+//
+//    }
 }//end of player class
--- src/Model/Referee.java
+++ src/Model/Referee.java
@@ -6,33 +6,62 @@ package Model;
  */
 public class Referee {
 
-    //Each Referee has to be assigned to each game
-
-    Card c = new Card();
+    Card c= new Card();
     String [] cardpackc;
+    String[][] playerCarray;
+    int playerscount;
 
-    public int StartGame(){
+    public Player[] StartGame(Player[] players){
+        playerscount = players.length;
+        //Player[] players = new Player[5];
         System.out.println("Game Started");
         cardpackc =c.Shuffel();
         System.out.println("cards=");
-        for (int i = 0; i < cardpackc.length; i++) {
+        /*for (int i = 0; i < cardpackc.length; i++) {
             System.out.print(cardpackc[i]+",");
-        }
+        }*/
         c.pushCard(cardpackc);
         System.out.println("pushed");
 
 
-        int playesrscout=5;// nede to send the players
-        return playesrscout;
+        //int playesrscout=4;// nede to send the players
+
+
+
+        playerCarray = DistributeCard(playerscount);
+
+        String[] usernames = storeUsernames(players);
+
+        for(int i =0; i<playerscount;i++){
+            players[i].getObjectReady(playerscount);
+            players[i].setNumberofplayers(playerscount);
+            players[i].setCardHand(playerCarray);
+            players[i].setUser_info(usernames);
+        }
+
+        playerCarray = null;
+        //FROM THIS POINT ON playerCarray will not be used, use the player object GetHand cards
+
+        players = calculateOverallScore(players);
+
+        return players;
     }
+
+    public String[] storeUsernames(Player[] players){
+        String[] usernames = new String[players.length] ;
+        for(int i =0; i< playerscount;i++){
+            usernames[i] = players[i].getUsername();
+        }
+        return usernames;
+    }
+
+
 
     public String[][] DistributeCard(int playerscount){
         //System.out.println("then"+c.pullcard());
-        String[][] playerCarray = new String [playerscount][5];
+        playerCarray = new String [playerscount][5];
 
         if (playerscount*5<=52) {
-
-            //j is player id and i is noOf cards each player has and its value
 
             for (int i=0;i<5;i++) {
 
@@ -60,4 +89,245 @@ public class Referee {
         return playerCarray;
     }
 
-}
+   /* public void CardExchange(String[] changecardlist, int playearno){
+
+        for (int i = 0; i < 3; i++) {
+            if (changecardlist[i]!="no"||changecardlist[i]!=null) {
+//                c.pushCard(changecardlist[i]);
+//                changecardlist[i]=c.pullcard();
+                for (int j = 0; j < 5; j++) {
+                    if (changecardlist[i]== playerCarray[playearno][j]) {
+                        c.pushCard(playerCarray[playearno][j]);
+                        playerCarray[playearno][j]=c.pullcard();
+                    }
+
+                }
+
+            }
+
+        }
+
+        System.out.println(""+changecardlist[1]);
+
+    }*/
+
+    public String swapPlayerCards(String currentCard){
+        c.pushCard(currentCard);
+        return c.pullcard();
+    }
+
+    public Player[] CardExchange(Player[] players){
+
+        for(int i = 0; i< playerscount;i++){
+            for(int j =0;j<5;j++){
+                if(players[i].getSwapCards()[j] ==true){
+                    String currentCard = players[i].getIndividualCardHand(i,j);
+                    currentCard = swapPlayerCards(currentCard);
+                    players[i].setIndividualCardHand(i, j, currentCard);
+                }
+
+
+            }
+        }
+        //NOTE INDIVDUAL GET HAND ON EACH PLAYER ISN'T ACCURATE
+        return players;
+    }
+
+    //declaring point array
+//   public  int[][] marks = { {20,19,18,17},
+//                          {19,18,17,16},
+//                          {18,17,16,15},
+//                          {17,16,15,14},
+//                          {16,15,14,13},
+//                          {15,14,13,12},
+//                          {14,13,12,11},
+//                          {12,11,10,9},
+//                          {11,10,9,8},
+//                          {10,9,8,7},
+//                          {9,8,7,6},
+//                          {8,7,6,5} };
+//
+//
+//
+//   int point=0;
+//   public int SetMarks(String card_type){
+//
+//       Player p=new Player();
+//      card_type=this.toString();
+//    switch(card_type){
+//
+//           case "s":
+//               point=5;
+//           break;
+//
+//           case "d":
+//               point=4;
+//           break;
+//
+//           case "c":
+//               point=3;
+//           break;
+//
+//           case "h":
+//               point=2;
+//           break;
+//
+//       }
+//
+//           System.out.println("point= "+point);
+//           return point;
+//
+//   }
+
+    public Player[] playerKick(int playerKickedId, Player[] players){
+        players[playerKickedId] = null;
+        Player[] playersNew = new Player[playerscount-1];
+        int count = 0;
+        for(int i = 0; i<playerscount;i++){
+            if(players[i]==null){
+                continue;
+            }
+            playersNew[count] = players[i];
+            count++;
+        }
+
+        return playersNew;
+
+    }
+
+    public Player[] calculateOverallScore(Player[] players){
+        int valueScore=0, typeScore=0, totalScore=0;
+        String [][] playerCardHand;
+        for(int i = 0;i<playerscount;i++){
+            playerCardHand = players[i].getCardHand();
+            typeScore = getTypeScore(i, playerCardHand);
+            valueScore = getValueScore(i, playerCardHand);
+            totalScore = typeScore + valueScore;
+            players[i].setScore(totalScore);
+        }
+
+        return players;
+    }
+
+    public int getTypeScore(int playerId, String[][] playerCardHand){
+
+
+        String type=null;
+//
+        //String[][] array=playerCarray;
+//       // array=displayHand();
+        int typeScore=0;
+        for(int i=0;i<5;i++){
+//
+            type=playerCardHand[playerId][i].substring(0,playerCardHand[playerId][i].indexOf("-"));
+//
+            System.out.println("user "+ playerId+" card type= "+type);
+
+            switch(type){
+
+                case "s":
+                    typeScore=5;
+                    //sum+=point;
+                    break;
+
+                case "d":
+                    typeScore=4;
+                    //sum+=point;
+                    break;
+
+                case "c":
+                    typeScore=3;
+                    //sum+=point;
+                    break;
+
+                case "h":
+                    typeScore=2;
+                    //sum+=point;
+                    break;
+
+            }
+
+
+        }
+
+        return typeScore;
+    }
+
+    public int getValueScore(int playerId, String[][] playerCardHand){
+
+
+        String value=null;
+        // String[][] array = playerCarray;
+        int valueScore = 0;
+        for(int i=0;i<5;i++){
+
+            value=playerCardHand[playerId][i].substring(playerCardHand[playerId][i].indexOf("-")+1);
+
+            System.out.println("user "+ playerId+ " card value= "+value);
+
+        }
+
+        switch(value){
+
+            case "A":
+                valueScore=15;
+                //sum+=point;
+                break;
+            case "K":
+                valueScore=14;
+                //sum+=point;
+                break;
+            case "Q":
+                valueScore=13;
+                //sum+=point;
+                break;
+            case "J":
+                valueScore=12;
+                //sum+=point;
+                break;
+            case "10":
+                valueScore=11;
+                //sum+=point;
+                break;
+            case "9":
+                valueScore=10;
+                //sum+=point;
+                break;
+            case "8":
+                valueScore=9;
+                //sum+=point;
+                break;
+            case "7":
+                valueScore=8;
+                //sum+=point;
+                break;
+            case "6":
+                valueScore=7;
+                //sum+=point;
+                break;
+            case "5":
+                valueScore=6;
+                //sum+=point;
+                break;
+            case "4":
+                valueScore=5;
+                //sum+=point;
+                break;
+
+            case "3":
+                valueScore=4;
+                //sum+=point;
+                break;
+
+            case "2":
+                valueScore=3;
+                //sum+=point;
+                break;
+
+
+
+        }
+
+        return valueScore;
+
+    }}
